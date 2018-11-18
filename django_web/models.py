541a0fb82aa1d295fd80fa43add460d8d572bcb0
link1996miky <512819631@qq.com>
2018-11-18 23:39:38 +0800

    update_orm

--- django_web/models.py
+++ django_web/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class user_info_one(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=30)
     email = models.EmailField()
     memo = models.TextField()
