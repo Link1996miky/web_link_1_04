65ba442f89f8b8822a35bfc5c79332d86c5c85f2
link1996miky <512819631@qq.com>
2018-11-28 00:48:29 +0800

    update_1128

--- django_web/models.py
+++ django_web/models.py
@@ -24,3 +24,73 @@ class user_info_one(models.Model):
 
     def __str__(self):
         return self.user_id
+
+
+class Author(models.Model):
+    man = "男"
+    women = "女"
+    sex_choices = (
+        (man, "男"),
+        (women, "女"),
+    )
+
+    author_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=30)
+    email = models.EmailField(blank=True)
+    memo = models.TextField(blank=True)
+    sex = models.CharField(
+        max_length=2,
+        choices=sex_choices,
+        default=man,
+    )
+
+    def __str__(self):
+        return self.author_id
+
+
+class Book(models.Model):
+    XH = "玄幻小说"
+    XX = "仙侠小说"
+    KH = "科幻小说"
+    BookType_choices = (
+        (XH, "玄幻"),
+        (XX, "仙侠"),
+        (KH, "科幻"),
+    )
+
+    Ongoing = "更新中"
+    TheEnd = "已完结"
+    Status_choices = (
+        (Ongoing, "更新"),
+        (TheEnd, "完结"),
+    )
+
+    book_name = models.CharField(max_length=30, unique=True)
+    author = models.ForeignKey(Author)
+    book_type = models.CharField(
+        max_length=8,
+        choices=BookType_choices,
+        default=XH,
+    )
+    book_sum = models.IntegerField()
+    book_memo = models.CharField(max_length=200, blank=True)
+    book_start_time = models.DateTimeField(auto_now_add=True)
+    book_update_time = models.DateTimeField(auto_now=True)
+    book_status = models.CharField(
+        max_length=6,
+        choices=Status_choices,
+        default=Ongoing,
+    )
+    book_img = models.ImageField(upload_to='img/')
+
+    def __str__(self):
+        return self.book_name
+
+
+class Book_Detail(models.Model):
+    book = models.ForeignKey(on_delete=models.CASCADE)
+    book_num = models.IntegerField()
+    book_file = models.FileField(upload_to='book/')
+
+
