dedfcae86f3cc52d122943ba1b41d90d81f3f499
link1996miky <512819631@qq.com>
2018-12-11 11:53:53 +0800

    update_1210

--- django_web/views.py
+++ django_web/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django_web import models
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+
 def home(request):
+    # 确认session中的用户是否存在
+    name = request.session.get('name', None)
+    if name:
+        return render(request, 'Home.html', {'name': name})
     return render(request, 'Home.html')
+
+
+def home_quit(request, quit_id):
+    if quit_id:
+        del request.session['name']
+        return render(request, 'Home.html',{'message': 'quit success'})
+    return render(request, 'Home.html', {'message': 'is not quit!'})
 
 
 def login(request):
@@ -15,14 +29,15 @@ def login(request):
             name = name.strip()
             password = password.strip()
             try:
-                user = models.user_info_one.objects.get(name=name)
+                user = models.user_info_one.objects.get(user_id=name)
                 if password == user.password:
+                    request.session['name'] = user.name
                     return redirect('/home')
                 else:
                     message = "密码错误"
             except:
                 message = "用户名不存在"
-        return render(request, "Login.html", {'message': message})
+            return render(request, "Login.html", {'message': message})
     return render(request, "Login.html")
 
 
@@ -40,3 +55,8 @@ def xianxia(request):
 
 def kehuan(request):
     return render(request, "Kehuan.html")
+
+
+@login_required
+def usr_message():
+    pass
